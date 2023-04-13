5e7a15a413e4ece8eae98286ab1a1f40234238dc
Artur <artur@51degrees.com>
2023-04-13 12:09:30 +0100

    WIP: adding scripts

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -4,6 +4,7 @@ param(
     [string]$Name
 )
 
-./java/run-integration-tests.ps1 -RepoName "device-detection-java" -ProjectDir $ProjectDir -Name $Name
+./java/run-integration-tests.ps1 -RepoName "device-detection-java-test" -ProjectDir $ProjectDir -Name $Name
 
 exit $LASTEXITCODE
+
