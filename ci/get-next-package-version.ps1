e8100ee5246601f20dd7fede280013019893381f
Artur <artur@51degrees.com>
2023-04-13 12:16:43 +0100

    WIP: Test

--- ci/get-next-package-version.ps1
+++ ci/get-next-package-version.ps1
@@ -3,6 +3,8 @@ param (
     [string]$VariableName
 )
 
-./java/get-next-package-version.ps1 -RepoName "device-detection-java-test" -VariableName "GitVersion"
+./steps/get-next-package-version.ps1 -RepoName "pipeline-java-test" -VariableName "GitVersion"
+
+Set-Variable -Name $VariableName -Value $GitVersion.SemVer -Scope Global
 
 exit $LASTEXITCODE
