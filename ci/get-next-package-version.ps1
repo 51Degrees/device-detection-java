05eee97d31a7708ba97d71bcbfae75c00121a1d5
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-04-18 09:17:41 +0100

    Update get-next-package-version.ps1

--- ci/get-next-package-version.ps1
+++ ci/get-next-package-version.ps1
@@ -3,8 +3,8 @@ param (
     [string]$VariableName
 )
 
-#./steps/get-next-package-version.ps1 -RepoName "device-detection-java-test" -VariableName "GitVersion"
+./steps/get-next-package-version.ps1 -RepoName "device-detection-java-test" -VariableName "GitVersion"
 
-Set-Variable -Name $VariableName -Value 4.4.21-gh-refact.1 -Scope Global
+Set-Variable -Name $VariableName -Value $GitVersion -Scope Global
 
 exit $LASTEXITCODE
