4826b96ed42eac8dda5ac31cdd2336698bf50461
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-04-20 16:40:16 +0100

    Update run-integration-tests.ps1

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -2,11 +2,11 @@
 param(
     [string]$ProjectDir = ".",
     [string]$Name = "Windows_Java_8",
-    [string]$PackageVersion = "0.0.0",
+    [string]$Version = "0.0.0",
     [Hashtable]$Keys
     
 )
 
-./java/run-integration-tests.ps1 -RepoName "de-detection-java-test" -ProjectDir $ProjectDir -Name $Name -PackageVersion $PackageVersion -Keys $Keys
+./java/run-integration-tests.ps1 -RepoName "de-detection-java-test" -ProjectDir $ProjectDir -Name $Name -Version $Version -Keys $Keys
 
 exit $LASTEXITCODE
