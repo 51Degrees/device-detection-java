2404b62c61175dcf26dffc5cc0982de27154d4bb
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-04-20 09:53:01 +0100

    Update fetch-assets.ps1

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -10,5 +10,5 @@ $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 
 ./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection
 
-Move-Item $RepoPath/TAC-HashV41.hash  $RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/TAC-HashV41.hash
+Copy-Item $RepoPath/TAC-HashV41.hash  $RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/TAC-HashV41.hash
 
