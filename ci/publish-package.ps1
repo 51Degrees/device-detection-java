6d34f179f0fa71e524e879f08a54681625747acf
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-04-27 11:39:15 +0100

    Update publish-package.ps1

--- ci/publish-package.ps1
+++ ci/publish-package.ps1
@@ -11,10 +11,12 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$CodeSigningCertAlias,
     [Parameter(Mandatory=$true)]
-    [string]$CodeSigningCertPassword
+    [string]$CodeSigningCertPassword,
+    [Parameter(Mandatory=$true)]
+    $Version
 )
 
-./java/publish-package-maven.ps1 -RepoName "de-detection-java-test" -MavenSettings $MavenSettings -JavaGpgKeyPassphrase $JavaGpgKeyPassphrase -CodeSigningCert $CodeSigningCert -JavaPGP $JavaPGP -CodeSigningCertAlias $CodeSigningCertAlias -CodeSigningCertPassword $CodeSigningCertPassword 
+./java/publish-package-maven.ps1 -RepoName "de-detection-java-test" -MavenSettings $MavenSettings -JavaGpgKeyPassphrase $JavaGpgKeyPassphrase -CodeSigningCert $CodeSigningCert -JavaPGP $JavaPGP -CodeSigningCertAlias $CodeSigningCertAlias -CodeSigningCertPassword $CodeSigningCertPassword -Version $Version
 
 
 exit $LASTEXITCODE
