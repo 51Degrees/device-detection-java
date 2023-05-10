e0c002a8e0dd89b5f9df9fed6433e61c619d5730
Artur <artur@51degrees.com>
2023-05-10 17:03:55 +0100

    FEAT: Pass resource key to the unit tests for cloud tests

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -1,7 +1,9 @@
 param(
     [string]$ProjectDir = "."
+    [string]$Name,
+    [hashtable]$Keys
 )
 
-./java/run-unit-tests.ps1 -RepoName "de-detection-java-test" -ProjectDir $ProjectDir -Name ""
+./java/run-unit-tests.ps1 -RepoName "de-detection-java-test" -ProjectDir $ProjectDir -Name $Name -ExtraArgs "-DTestResourceKey=$($Keys.TestResourceKey)"
 
 exit $LASTEXITCODE
