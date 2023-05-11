3da06f27fa3c2135df041768bc60b0c65bb02e7f
Artur <artur@51degrees.com>
2023-05-11 11:55:10 +0100

    CLEANUP: Remove download from unit tests. BUG: Correct path to evidence files

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -4,9 +4,6 @@ param(
     [hashtable]$Keys
 )
 
-Write-Output "Download Lite file"
-curl -L -o "de-detection-java-test/51Degrees-LiteV4.1.hash" "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash"
-
 ./java/run-unit-tests.ps1 -RepoName "de-detection-java-test" -ProjectDir $ProjectDir -Name $Name -ExtraArgs "-DTestResourceKey=$($Keys.TestResourceKey)"
 
 exit $LASTEXITCODE
