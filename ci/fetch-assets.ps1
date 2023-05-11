df6ce9d8d3822ae6d7b8c437fccd0cb41d0dc48c
Artur <artur@51degrees.com>
2023-05-11 11:48:22 +0100

    REFACT: Change dir of assets

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -14,15 +14,15 @@ Write-Output "Download Lite file"
 curl -L -o "$RepoPath/51Degrees-LiteV4.1.hash" "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash"
 
 Write-Output "Download Evidence file"
-curl -o "$RepoPath/device-detection-data/20000 Evidence Records.yml" "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20Evidence%20Records.yml"    
+curl -o "$RepoPath/20000 Evidence Records.yml" "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20Evidence%20Records.yml"    
 
 Write-Output "Download User Agents file"
-curl -o "$RepoPath/device-detection-data/20000 User Agents.csv" "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20User%20Agents.csv"
+curl -o "$RepoPath/20000 User Agents.csv" "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20User%20Agents.csv"
 
 
-Copy-Item "$RepoPath/device-detection-data/20000 Evidence Records.yml"  "$RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/20000 Evidence Records.yml"
+Copy-Item "$RepoPath/20000 Evidence Records.yml"  "$RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/20000 Evidence Records.yml"
 
-Copy-Item "$RepoPath/device-detection-data/20000 User Agents.csv"  "$RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/20000 User Agents.csv"
+Copy-Item "$RepoPath/20000 User Agents.csv"  "$RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/20000 User Agents.csv"
 
 Copy-Item $RepoPath/51Degrees-LiteV4.1.hash  $RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/51Degrees-LiteV4.1.hash
 
