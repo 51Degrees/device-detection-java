f8e0a6ddf0f1534ed46d3c3990c26d03a2b17e46
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-15 17:12:58 +0100

    Create update-packages.ps1

---
+++ ci/update-packages.ps1
@@ -0,0 +1,11 @@
+
+param(
+    [string]$ProjectDir = ".",
+    [string]$Name,
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName
+)
+
+./java/run-update-dependencies.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name
+
+exit $LASTEXITCODE
