eabbc1b358cee8c43f05ce4e5dfd9a042fedc204
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-15 09:42:22 +0100

    Update publish-package.ps1

--- ci/publish-package.ps1
+++ ci/publish-package.ps1
@@ -1,15 +1,16 @@
 
 param(
     [Parameter(Mandatory=$true)]
-    [string]$MavenSettings,
+    [Hashtable]$Keys,
     [Parameter(Mandatory=$true)]
     $Version,
     [Parameter(Mandatory=$true)]
     [string]$RepoName
+    
 
 )
 
-./java/publish-package-maven.ps1 -RepoName $RepoName -MavenSettings $MavenSettings -Version $Version
+./java/publish-package-maven.ps1 -RepoName $RepoName -MavenSettings $Keys['MavenSettings'] -Version $Version
 
 
 exit $LASTEXITCODE
