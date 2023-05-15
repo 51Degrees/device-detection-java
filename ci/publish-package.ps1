da2af8bdd80f3b371dadf925ba4078ee974b1ca4
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-15 09:41:35 +0100

    Update publish-package.ps1

--- ci/publish-package.ps1
+++ ci/publish-package.ps1
@@ -3,11 +3,13 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$MavenSettings,
     [Parameter(Mandatory=$true)]
-    $Version
+    $Version,
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName
 
 )
 
-./java/publish-package-maven.ps1 -RepoName "de-detection-java-test" -MavenSettings $MavenSettings -Version $Version
+./java/publish-package-maven.ps1 -RepoName $RepoName -MavenSettings $MavenSettings -Version $Version
 
 
 exit $LASTEXITCODE
