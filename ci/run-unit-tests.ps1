fcdbc06bcc92cf4097d585cfd91fb362f4a97e1a
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-15 09:46:11 +0100

    Update run-unit-tests.ps1

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -1,9 +1,11 @@
 param(
     [string]$ProjectDir = ".",
     [string]$Name,
-    [hashtable]$Keys
+    [hashtable]$Keys,
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName
 )
 
-./java/run-unit-tests.ps1 -RepoName "de-detection-java-test" -ProjectDir $ProjectDir -Name $Name -ExtraArgs "-DTestResourceKey=$($Keys.TestResourceKey)"
+./java/run-unit-tests.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -ExtraArgs "-DTestResourceKey=$($Keys.TestResourceKey)"
 
 exit $LASTEXITCODE
