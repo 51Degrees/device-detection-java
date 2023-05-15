1204f95bc5c53080d9810ac4c3deada58d0d2c84
Artur51D <106963910+Artur51D@users.noreply.github.com>
2023-05-15 09:30:26 +0100

    Update build-project.ps1

--- ci/build-project.ps1
+++ ci/build-project.ps1
@@ -1,10 +1,12 @@
 param(
     [string]$ProjectDir = ".",
-    [string]$Name
+    [string]$Name,
+    [Parameter(Mandatory=$true)]
+    [string]$RepoName
 )
 
 Write-Output $ENV:JAVA_HOME
 
-./java/build-project.ps1 -RepoName "de-detection-java-test" -ProjectDir $ProjectDir -Name $Name
+./java/build-project.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name
 
 exit $LASTEXITCODE
