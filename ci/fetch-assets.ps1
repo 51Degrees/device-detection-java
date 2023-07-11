2ae6a36bf3a2e5c08e9f1864aa85d9f66b6e843e
ben51degrees <ben@51degrees.com>
2023-07-11 12:37:35 +0100

    Update fetch-assets.ps1

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -2,18 +2,14 @@
 param (
     [Parameter(Mandatory=$true)]
     [string]$RepoName,
+    [Parameter(Mandatory=$true)]
     [string]$DeviceDetection,
     [string]$DeviceDetectionUrl
 )
 
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 
-if ($DeviceDetection -ne "") {
-    ./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
-}
-else {
-    Write-Warning "No device detection key provided. Not downloading Hash file"
-}
+./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
 
 Write-Output "Download Lite file"
 curl -L -o "$RepoPath/51Degrees-LiteV4.1.hash" "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash"
@@ -31,7 +27,4 @@ Copy-Item "$RepoPath/20000 User Agents.csv"  "$RepoPath/device-detection.hash.en
 
 Copy-Item $RepoPath/51Degrees-LiteV4.1.hash  $RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/51Degrees-LiteV4.1.hash
 
-if ($DeviceDetection != $Null) {
-    Copy-Item $RepoPath/TAC-HashV41.hash  $RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/TAC-HashV41.hash
-}
-
+Copy-Item $RepoPath/TAC-HashV41.hash  $RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/TAC-HashV41.hash
