0a17ff70d1f35fd909b5df6cd9434ffa87d39608
ben51degrees <ben@51degrees.com>
2023-07-11 16:37:56 +0100

    Update fetch-assets.ps1

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -15,13 +15,13 @@ else {
     Write-Warning "A device detection license was not provided. So Hash data file will not be downloaded."
 }
 Write-Output "Download Lite file"
-curl -L -o "$RepoPath/51Degrees-LiteV4.1.hash" "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash"
+curl -L -o "$RepoPath/51Degrees-LiteV4.1.hash" "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash" -s
 
 Write-Output "Download Evidence file"
-curl -o "$RepoPath/20000 Evidence Records.yml" "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20Evidence%20Records.yml"    
+curl -o "$RepoPath/20000 Evidence Records.yml" "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20Evidence%20Records.yml" -s
 
 Write-Output "Download User Agents file"
-curl -o "$RepoPath/20000 User Agents.csv" "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20User%20Agents.csv"
+curl -o "$RepoPath/20000 User Agents.csv" "https://media.githubusercontent.com/media/51Degrees/device-detection-data/main/20000%20User%20Agents.csv" -s
 
 
 Copy-Item "$RepoPath/20000 Evidence Records.yml"  "$RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/20000 Evidence Records.yml"
