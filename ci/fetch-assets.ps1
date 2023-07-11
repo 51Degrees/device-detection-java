09e5f471c60216fd488bc274d49d73c3117cb97b
ben51degrees <ben@51degrees.com>
2023-07-11 13:43:02 +0100

    Update fetch-assets.ps1

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -11,6 +11,9 @@ $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 if ($DeviceDetection -ne "") {
     ./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl
 }
+else {
+    Write-Warning "A device detection license was not provided. So Hash data file will not be downloaded."
+}
 Write-Output "Download Lite file"
 curl -L -o "$RepoPath/51Degrees-LiteV4.1.hash" "https://github.com/51Degrees/device-detection-data/raw/main/51Degrees-LiteV4.1.hash"
 
@@ -27,4 +30,6 @@ Copy-Item "$RepoPath/20000 User Agents.csv"  "$RepoPath/device-detection.hash.en
 
 Copy-Item $RepoPath/51Degrees-LiteV4.1.hash  $RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/51Degrees-LiteV4.1.hash
 
-Copy-Item $RepoPath/TAC-HashV41.hash  $RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/TAC-HashV41.hash
+if ($DeviceDetection -ne "") {
+    Copy-Item $RepoPath/TAC-HashV41.hash  $RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/TAC-HashV41.hash
+}
