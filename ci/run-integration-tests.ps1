beae484209bf642b237e1ad9d0301027319e59d2
ben51degrees <ben@51degrees.com>
2023-07-12 11:46:15 +0100

    Update run-integration-tests.ps1

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -30,6 +30,7 @@ try {
         Copy-Item $TacFile device-detection-java-examples/device-detection-data/TAC-HashV41.hash
     }
     else {
+        Copy-Item $RepoPath/device-detection.hash.engine.on-premise/src/main/cxx/device-detection-cxx/device-detection-data/51Degrees-LiteV4.1.hash device-detection-java-examples/device-detection-data/51Degrees-LiteV4.1.hash
         Write-Output "::warn file=$(Get-CurrentFileName),line=$(Get-CurrentLineNumber),endLine=$(Get-CurrentLineNumber),title=No On-Premise Data File::No on-premise license was provided, so some tests will not run."
     }
     
