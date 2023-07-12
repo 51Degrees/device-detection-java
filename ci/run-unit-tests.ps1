5047eee93cec1faa68e16e10f625ed698dec5344
ben51degrees <ben@51degrees.com>
2023-07-12 13:14:38 +0100

    Update run-unit-tests.ps1

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -6,6 +6,9 @@ param(
     [string]$RepoName
 )
 
+if ($Keys.TestResourceKey -eq "") {
+    Write-Output "::warning file=$(Get-CurrentFileName),line=$(Get-CurrentLineNumber),endLine=$(Get-CurrentLineNumber),title=No Resource Key::No resource key was provided, so cloud tests will not run."
+}
 ./java/run-unit-tests.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -ExtraArgs "-DTestResourceKey=$($Keys.TestResourceKey)"
 
 exit $LASTEXITCODE
