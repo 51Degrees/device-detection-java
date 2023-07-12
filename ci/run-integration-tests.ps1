9dfe80a26a627993abdc686259d7e6c936a2e7cf
ben51degrees <ben@51degrees.com>
2023-07-12 13:14:11 +0100

    Update run-integration-tests.ps1

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -35,7 +35,7 @@ try {
     }
 
     if ($Keys.TestResourceKey -eq "") {
-        Write-Output "::warning file=$(Get-CurrentFileName),line=$(Get-CurrentLineNumber),endLine=$(Get-CurrentLineNumber),title=No resource key was provided, so cloud tests will not run."
+        Write-Output "::warning file=$(Get-CurrentFileName),line=$(Get-CurrentLineNumber),endLine=$(Get-CurrentLineNumber),title=No Resource Key::No resource key was provided, so cloud tests will not run."
     }
     
     Write-Output "Moving evidence files for examples"
