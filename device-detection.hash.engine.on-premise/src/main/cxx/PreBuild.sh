5e87199ae92f1abb6d9fc7e444a8f14ee9254494
Alexander Popel <alexander.popel@postindustria.com>
2023-08-21 15:42:08 +0300

    Build ARM64 binary as well on macOS

--- device-detection.hash.engine.on-premise/src/main/cxx/PreBuild.sh
+++ device-detection.hash.engine.on-premise/src/main/cxx/PreBuild.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 
 # mvn clean will delete target so that's the place to be building
 mkdir -p target/cxx-build
@@ -10,3 +11,11 @@ cmake ../../src/main/cxx
 # put output in target/classes so that it will get jarred up
 echo "cmake build"
 cmake --build . --target fiftyone-hash-java
+
+if [[ $OSTYPE == darwin* ]]; then
+	echo "additional ARM64 build"
+	echo "configure"
+	cmake ../../src/main/cxx -DARCH=aarch64 -DCMAKE_OSX_ARCHITECTURES=arm64 -DBUILD_TESTING=OFF
+	echo "cmake build"
+	cmake --build . --target fiftyone-hash-java
+fi
