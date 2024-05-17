48a091a6393f9bc03f846df67cc7f3b499116754
Eugene Dorfman <eugene.dorfman@gmail.com>
2024-05-17 10:30:12 +0200

    fix: ValueTests use a Chrome user-agent
    * to verify all properties including javascript snippets

--- device-detection.shared/src/test/java/fiftyone/devicedetection/shared/testhelpers/data/ValueTests.java
+++ device-detection.shared/src/test/java/fiftyone/devicedetection/shared/testhelpers/data/ValueTests.java
@@ -86,12 +86,13 @@ public class ValueTests {
     @SuppressWarnings("unchecked")
     public static void valueTypes(Wrapper wrapper) throws Exception {
         try (FlowData data = wrapper.getPipeline().createFlowData()) {
-            data.addEvidence("header.user-agent", Constants.MobileUserAgent)
+            data.addEvidence("header.user-agent",
+                            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
                 .process();
             ElementData elementData = data.get(wrapper.getEngine().getElementDataKey());
             for (FiftyOneAspectPropertyMetaData property :
                 (List<FiftyOneAspectPropertyMetaData>) wrapper.getEngine().getProperties()) {
-                if (property.isAvailable() && !Objects.equals(property.getName(), "JavascriptGetHighEntropyValues")) {
+                if (property.isAvailable() ) {
                     Class<?> expectedType;
                     Object value = elementData.get(property.getName());
                     //((ElementPropertyMetaData) value).getType();
