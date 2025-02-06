174bba98f9b54326789e2c0a539e3539489de5e2
Alexander Popel <74662418+pi-alexander-popel@users.noreply.github.com>
2025-02-06 07:22:43 +0200

    Merged Pull Request '#278 linux-arm-build->main: ci: switch to older Ubuntu, run on ARM as well'
    Note: JavaSDKEnvVar in Ubuntu ARM is not wrong. Its value is currently
    `/usr/lib/jvm/temurin-11-jdk-arm64` despite having X64 suffix.

--- device-detection.hash.engine.on-premise/src/main/cxx/PreBuild.sh
+++ device-detection.hash.engine.on-premise/src/main/cxx/PreBuild.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eu
 
+if [[ $OSTYPE == linux* && $(uname -m) == aarch64 ]] && command -v zig >/dev/null; then
+	echo "using zig cc to target GLIBC 2.26"
+	export CC="zig cc -target aarch64-linux-gnu.2.26 -s" CXX="zig c++ -target aarch64-linux-gnu.2.26 -s"
+	export CMAKE_LIBRARY_PATH=/lib/aarch64-linux-gnu
+fi
+
 # mvn clean will delete target so that's the place to be building
 mkdir -p target/cxx-build
 mkdir target/classes
@@ -18,13 +24,4 @@ if [[ $OSTYPE == darwin* ]]; then
 	cmake ../../src/main/cxx -DARCH=aarch64 -DCMAKE_OSX_ARCHITECTURES=arm64 -DBUILD_TESTING=OFF
 	echo "cmake build"
 	cmake --build . --target fiftyone-hash-java
-elif [[ $OSTYPE == linux* ]] && command -v zig >/dev/null; then
-	echo "additional ARM64 build"
-	export CC="zig cc -target aarch64-linux-gnu.2.26 -s" CXX="zig c++ -target aarch64-linux-gnu.2.26 -s"
-	echo "configure"
-	sed -i '/^if (NOT MSVC AND NOT APPLE)/,/^endif()/s/^/#/' ../../src/main/cxx/device-detection-cxx/src/common-cxx/CMakeLists.txt # 🤡
-	cmake ../../src/main/cxx -DARCH=aarch64 -DBUILD_TESTING=OFF --fresh
-	echo "cmake build"
-	cmake --build . --target fiftyone-hash-java --clean-first
-	sed -i '/^#if (NOT MSVC AND NOT APPLE)/,/^#endif()/s/^#//' ../../src/main/cxx/device-detection-cxx/src/common-cxx/CMakeLists.txt # 🤡
 fi
