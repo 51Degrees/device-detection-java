90edbfb02d6553ea4c4bcfb9a481c96a6793d0b2
Maksym Kucherov <maximus.the0@gmail.com>
2025-02-07 13:31:35 -0500

    Revert "Merged Pull Request '#259 feat/add-evidence-keys->version/4.5: add special evidence into evidence key filter'"
    This reverts commit 48f3900f8cc14604a55e407f4bd6369b07fbdd6c, reversing
    changes made to 604aa8430d4572567ff4609b89e90ebf034327e2.

--- device-detection.hash.engine.on-premise/src/test/java/fiftyone/devicedetection/hash/engine/onpremise/flowelements/EngineTests.java
+++ device-detection.hash.engine.on-premise/src/test/java/fiftyone/devicedetection/hash/engine/onpremise/flowelements/EngineTests.java
@@ -31,6 +31,7 @@ import org.junit.Test;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.LoggerFactory;
 
+import fiftyone.devicedetection.shared.testhelpers.FileUtils;
 import fiftyone.pipeline.engines.Constants;
 import static fiftyone.devicedetection.shared.testhelpers.FileUtils.LITE_HASH_DATA_FILE_NAME;
 import static fiftyone.devicedetection.shared.testhelpers.FileUtils.TAC_HASH_DATA_FILE_NAME;
@@ -41,70 +42,56 @@ public class EngineTests {
 
 	private DeviceDetectionHashEngine createEngine(File dataFile) throws Exception {
 		return new DeviceDetectionHashEngineBuilder(loggerFactory, null)
-				.setPerformanceProfile(Constants.PerformanceProfiles.HighPerformance)
-				.setUpdateMatchedUserAgent(true)
-				.setAutoUpdate(false)
-				.build(dataFile.toString(), false);
+                .setPerformanceProfile(Constants.PerformanceProfiles.HighPerformance)
+                .setUpdateMatchedUserAgent(true)
+                .setAutoUpdate(false)
+                .build(dataFile.toString(), false);
 	}
-
+	
 	/**
 	 * Shared method for dataDataSourceTier tests.
-	 *
 	 * @param fileName
 	 * @throws Exception
 	 */
 	private void testDataSourceTier(String fileName) throws Exception {
-		DeviceDetectionHashEngine engine = createEngine(fileName);
-		assertTrue(engine.getEvidenceKeyFilter().include(
-				fiftyone.devicedetection.hash.engine.onpremise.flowelements.Constants.EVIDENCE_QUERY_GHEV));
-		assertTrue(engine.getEvidenceKeyFilter().include(
-				fiftyone.devicedetection.hash.engine.onpremise.flowelements.Constants.EVIDENCE_COOKIE_GHEV));
-		assertTrue(engine.getEvidenceKeyFilter().include(
-				fiftyone.devicedetection.hash.engine.onpremise.flowelements.Constants.EVIDENCE_QUERY_SUA));
-		assertTrue(engine.getEvidenceKeyFilter().include(
-				fiftyone.devicedetection.hash.engine.onpremise.flowelements.Constants.EVIDENCE_COOKIE_SUA));
-
+		File dataFile = null;
+		try {
+			dataFile = getFilePath(fileName);
+		}
+		catch (IllegalArgumentException e) {
+			assumeTrue(e.getMessage(), false);
+		}
+		
+		DeviceDetectionHashEngine engine = createEngine(dataFile);
 		String tier = engine.getDataSourceTier();
 		engine.close();
-
+		
 		if (fileName.equals(LITE_HASH_DATA_FILE_NAME)) {
 			assertEquals("Lite", tier);
-		} else {
+		}
+		else {
 			assertTrue(tier.equalsIgnoreCase("Enterprise") ||
 					tier.equalsIgnoreCase("TAC"));
 		}
 	}
-
-	private DeviceDetectionHashEngine createEngine(String fileName) throws Exception {
-		File dataFile = null;
-		try {
-			dataFile = getFilePath(fileName);
-		} catch (IllegalArgumentException e) {
-			assumeTrue(e.getMessage(), false);
-		}
-
-		DeviceDetectionHashEngine engine = createEngine(dataFile);
-		return engine;
-	}
-
+	
 	/**
 	 * Check that getDataSourceTier returns correct product for Lite data file
-	 *
 	 * @throws Exception
 	 */
 	@Test
 	public void Engine_Hash_GetDataSourceTier_Lite() throws Exception {
 		testDataSourceTier(LITE_HASH_DATA_FILE_NAME);
 	}
-
+	
 	/**
 	 * Check that getDataSourceTier returns correct product for Enterprise data
 	 * file
-	 *
 	 * @throws Exception
 	 */
 	@Test
 	public void Engine_Hash_GetDataSourceTier_Enterprise() throws Exception {
 		testDataSourceTier(TAC_HASH_DATA_FILE_NAME);
 	}
+
 }
