b4610c2b58c95cdcf58a24e05c757e8e9c998350
Alexander Popel <alexander.popel@postindustria.com>
2025-02-11 14:40:36 +0200

    ci: use zig for glibc cross-compilation on x86 too
    Zig is downloaded from common-ci releases mirror to avoid straining
    official/community servers.

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -5,6 +5,15 @@ param(
     [string]$JavaSDKEnvVar,
     [string]$ProjectDir = "."
 )
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+
+if ($IsLinux -and -not (Get-Command zig -ErrorAction SilentlyContinue)) {
+    Write-Host "Installing zig for glibc cross-compilation..."
+    New-Item -ItemType Directory -Force ~/.local/bin, ~/.local/opt/zig | Out-Null
+    curl -sSL --fail-with-body 'https://github.com/51Degrees/common-ci/releases/download/zig/zig-linux-x86_64-0.13.0.tar.xz' | tar -xJ --strip-components=1 -C ~/.local/opt/zig
+    New-Item -ItemType SymbolicLink -Path ~/.local/bin/zig -Target ~/.local/opt/zig/zig | Out-Null
+}
 
 ./java/setup-enviroment.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -JavaSDKEnvVar $JavaSDKEnvVar
 
