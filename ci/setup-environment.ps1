6f41212e47cbf900b7641b679357931f302f9da6
Alexander Popel <alexander.popel@postindustria.com>
2025-03-12 13:27:06 +0200

    setup-environment: install aarch64 zig on aarch64

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -11,8 +11,15 @@ $PSNativeCommandUseErrorActionPreference = $true
 if ($IsLinux -and -not (Get-Command zig -ErrorAction SilentlyContinue)) {
     Write-Host "Installing zig for glibc cross-compilation..."
     New-Item -ItemType Directory -Force ~/.local/bin, ~/.local/opt/zig | Out-Null
-    curl -sSL --fail-with-body 'https://github.com/51Degrees/common-ci/releases/download/zig/zig-linux-x86_64-0.13.0.tar.xz' | tar -xJ --strip-components=1 -C ~/.local/opt/zig
-    New-Item -ItemType SymbolicLink -Path ~/.local/bin/zig -Target ~/.local/opt/zig/zig | Out-Null
+    $url = switch ([System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture) {
+        'X64' {'https://github.com/51Degrees/common-ci/releases/download/zig/zig-linux-x86_64.tar.xz'}
+        'Arm64' {'https://github.com/51Degrees/common-ci/releases/download/zig/zig-linux-aarch64.tar.xz'}
+        default {Write-Error "Unimplemented architecture: $_"}
+    }
+    curl -sSL --fail-with-body $url | tar -xJ --strip-components=1 -C ~/.local/opt/zig
+    # Don't try to use ~ instead of $HOME here
+    New-Item -ItemType SymbolicLink -Path $HOME/.local/bin/zig -Target $HOME/.local/opt/zig/zig | Out-Null
+    zig version
 }
 
 ./java/setup-enviroment.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -JavaSDKEnvVar $JavaSDKEnvVar
