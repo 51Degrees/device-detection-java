4cb92e09358aa9c477d474c5818514c3f2e50543
Alexander Popel <alexander.popel@postindustria.com>
2025-11-25 14:02:13 +0200

    ci: update generate-accessors parameters

--- ci/generate-accessors.ps1
+++ ci/generate-accessors.ps1
@@ -1,10 +1,11 @@
 param (
     [Parameter(Mandatory)][string]$RepoName,
-    [string]$DataFile = "$RepoName/TAC-HashV41.hash"
+    [string]$MetaDataPath = "$PWD/common-metadata",
+    [string]$DataType = "HashV41"
 )
 $ErrorActionPreference = "Stop"
 
-./tools/ci/generate-accessors.ps1 @PSBoundParameters
+./tools/ci/generate-accessors.ps1 -RepoName:$RepoName -MetaDataPath:$MetaDataPath -DataType:$DataType
 
 Copy-Item "tools/Java/DeviceDataBase.java" "device-detection-java/device-detection.shared/src/main/java/fiftyone/devicedetection/shared/"
 Copy-Item "tools/Java/DeviceData.java" "device-detection-java/device-detection.shared/src/main/java/fiftyone/devicedetection/shared/"
