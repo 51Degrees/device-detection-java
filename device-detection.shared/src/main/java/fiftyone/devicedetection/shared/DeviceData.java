00f77be57422da9a9e311072afde486e7bed5690
Automation51D <60396166+Automation51D@users.noreply.github.com>
2026-01-29 19:30:21 +0100

    Merged Pull Request '#497 properties-update/main->main: Update properties'
    Co-authored-by: Automation51D <51DCI@51Degrees.com>

--- device-detection.shared/src/main/java/fiftyone/devicedetection/shared/DeviceData.java
+++ device-detection.shared/src/main/java/fiftyone/devicedetection/shared/DeviceData.java
@@ -729,7 +729,7 @@ public interface DeviceData extends AspectData
 	 */
 	AspectPropertyValue<String> getNativePlatform();
 	/**
-	 * Indicates the number of screens the device has. This property is not applicable for a device that does not have a screen.
+	 * Indicates the number of screens the device has. This property is not applicable for a device that does not have a screen. A display is only considered a screen if it could be used to display a web page.
 	 */
 	AspectPropertyValue<Integer> getNumberOfScreens();
 	/**
