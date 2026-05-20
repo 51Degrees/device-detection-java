5c73d49c5960c5630b4ba60e0028df09e4a9bd97
Automation51D <60396166+Automation51D@users.noreply.github.com>
2026-05-20 18:48:08 +0100

    Merged Pull Request '#531 update-packages/main->main: Update packages'
    * Update packages
    
    * do not fail on cloud error message text change
    
    * update pipeline-java to 4.5.7
    
    * adapt tests to cloud v5 - user-agents are now 4
    
    see https://github.com/51Degrees/device-detection-cxx/issues/362
    
    * remove extra params from generate-accessors script
    
    ---------
    
    Co-authored-by: Automation51D <51DCI@51Degrees.com>
    Co-authored-by: Eugene Dorfman <eugene.dorfman@gmail.com>

--- ci/generate-accessors.ps1
+++ ci/generate-accessors.ps1
@@ -1,11 +1,10 @@
 param (
     [Parameter(Mandatory)][string]$RepoName,
-    [string]$MetaDataPath = "$PWD/common-metadata",
     [string]$DataType = "HashV41"
 )
 $ErrorActionPreference = "Stop"
 
-./tools/ci/generate-accessors.ps1 -RepoName:$RepoName -MetaDataPath:$MetaDataPath -DataType:$DataType
+./tools/ci/generate-accessors.ps1 -DataType:$DataType
 
 Copy-Item "tools/Java/DeviceDataBase.java" "device-detection-java/device-detection.shared/src/main/java/fiftyone/devicedetection/shared/"
 Copy-Item "tools/Java/DeviceData.java" "device-detection-java/device-detection.shared/src/main/java/fiftyone/devicedetection/shared/"
